bbb9409e68bd571195d28747f27bc57dc12a245a
VagizAitov <aitov-vagiz@mail.ru>
2025-09-27 16:55:03 +0400

    comment test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <img src="" alt="" />
+    <img src="https://media1.thrillophilia.com/filestore/uwpz857lua13qmvub6um2v93dlrm_IMG%20Worlds%20%20of%20Adventure.jpg" alt="" />
     </>
   )
 }
